1803f48addb74c45bb16c57efd5b69b325ac1c9a
WandaElizabeth <wandawairimu@gmail.com>
2022-02-16 19:55:25 +0300

    first assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var name:String="Cathrine Wanjiru"
+    println(name)
+   var age:Byte = 40
+    println(age)
+    var phonenumber="0797984396"
+    println(phonenumber)
+    var weight="40.3kg"
+     println(weight)
+    var Kenyan:Boolean=true
+   println(Kenyan)
+}
